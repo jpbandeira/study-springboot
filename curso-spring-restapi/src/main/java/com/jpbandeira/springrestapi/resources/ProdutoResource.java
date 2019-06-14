a415dbd57599e1d7a465868fde815c5760c3f33a
jpbandeira <pedrobandeira@vialogicasistemas.com.br>
2019-06-14 08:38:40 -0300

    Criando classes service e resource para produto.

---
+++ curso-spring-restapi/src/main/java/com/jpbandeira/springrestapi/resources/ProdutoResource.java
@@ -0,0 +1,48 @@
+package com.jpbandeira.springrestapi.resources;
+
+import com.jpbandeira.springrestapi.domain.Produto;
+import com.jpbandeira.springrestapi.services.ProdutoService;
+import javassist.tools.rmi.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/produtos")
+public class ProdutoResource {
+	
+	@Autowired
+	private ProdutoService produtoService;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<?> find(@PathVariable Long id) throws ObjectNotFoundException {
+		Optional<Produto> objetoProduto = Optional.ofNullable(produtoService.find(id));
+		return ResponseEntity.ok().body(objetoProduto);
+	}
+
+
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody Produto objetoProduto){
+		objetoProduto = produtoService.insert(objetoProduto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(objetoProduto.getId()).toUri();
+
+		return ResponseEntity.created(uri).build();
+	}
+
+	@PutMapping
+	public ResponseEntity<Void> update(@RequestBody Produto objetoProduto){
+		System.out.println(objetoProduto.toString());
+		produtoService.update(objetoProduto);
+		return ResponseEntity.noContent().build();
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		produtoService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+}
---
+++ curso-spring-restapi/src/main/java/com/jpbandeira/springrestapi/services/ProdutoService.java
@@ -0,0 +1,34 @@
+package com.jpbandeira.springrestapi.services;
+
+import com.jpbandeira.springrestapi.domain.Produto;
+import com.jpbandeira.springrestapi.repositories.ProdutoRepository;
+import javassist.tools.rmi.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ProdutoService {
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
+
+	public Produto find(Long id) throws ObjectNotFoundException {
+		Optional<Produto> objetoProduto = produtoRepository.findById(id);
+		return objetoProduto.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Produto.class.getName()));
+	}
+
+    public Produto insert(Produto objetoProduto) {
+		objetoProduto.setId(null);
+		return produtoRepository.save(objetoProduto);
+    }
+
+	public Produto update(Produto objetoProduto){
+		return produtoRepository.save(objetoProduto);
+	}
+
+	public void delete(Long id) {
+		produtoRepository.deleteById(id);
+	}
+}
