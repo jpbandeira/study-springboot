a88e9b3a81de81b82f15a14ecf486ec156a22f96
pedrobandeira <joaopedro980804@gmail.com>
2019-07-16 11:20:26 -0300

    AJuste feito para gravar cidade e estado automaticamente no banco de dados

--- curso-spring-restapi/src/main/java/com/jpbandeira/springrestapi/CursoSpringRestapiApplication.java
+++ curso-spring-restapi/src/main/java/com/jpbandeira/springrestapi/CursoSpringRestapiApplication.java
@@ -1,12 +1,32 @@
 package com.jpbandeira.springrestapi;
 
+
+import com.jpbandeira.springrestapi.domain.Cidade;
+import com.jpbandeira.springrestapi.domain.Estado;
+import com.jpbandeira.springrestapi.repositories.CidadeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class CursoSpringRestapiApplication{
+public class CursoSpringRestapiApplication implements CommandLineRunner {
+
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringRestapiApplication.class, args);
 	}
-}
+
+	@Override
+	public void run(String... args) throws  Exception{
+			Estado estado = new Estado(null, "CE");
+			Cidade cidade = new Cidade(null, "Fortaleza", estado);
+
+			cidadeRepository.saveAll(Arrays.asList(cidade));
+		}
+	}
+
--- curso-spring-restapi/src/main/java/com/jpbandeira/springrestapi/domain/Cliente.java
+++ curso-spring-restapi/src/main/java/com/jpbandeira/springrestapi/domain/Cliente.java
@@ -22,6 +22,7 @@ public class Cliente  implements Serializable {
     @Length(min = 5, max = 120, message = "O tamanho deve ser entre 5 e 120 caracteres")
     @NotEmpty(message = "Preenchimento obrigatório")
     private String nome;
+    @Column(unique = true)
     private String email;
     private String cpfOuCnpj;
     /*Nessea atributo não sera armazenado um dado tipo cliente, mas sim um dado do tipo inteiro
