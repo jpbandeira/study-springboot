3eb73f3b85aca55f40f14b951d7179e947d43d5b
jpbandeira <jpbandeiralima@gmail.com>
2019-09-10 11:20:23 -0300

    Incluindo notação transactional

--- curso-spring-restapi/src/main/java/com/jpbandeira/springrestapi/repositories/ProdutoRepository.java
+++ curso-spring-restapi/src/main/java/com/jpbandeira/springrestapi/repositories/ProdutoRepository.java
@@ -7,10 +7,12 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+    @Transactional(readOnly = true)
     Page<Produto> findDistinctByNomeContainingAndCategoriaIn(String nome, List<Categoria> categorias, Pageable pageRequest);
 }
